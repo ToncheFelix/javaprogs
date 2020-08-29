ade6211133a9d354c9c7a50571817290df283f87
ToncheFelix <tonchefelix@gmail.com>
2020-08-29 04:53:13 -0500

    agregando con encabezados

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,12 @@
+/**
+* 
+* Calculator v1.0
+* Zacatecas, MX
+* 
+* @author  Felix Tonche Valadez
+* @version 1.0
+* @since   2017-10-21 
+*/
 import javax.swing.*;
 import javax.swing.JFrame;
 import java.awt.event.*;
